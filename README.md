5364a5ab8a71fe46f183d16a03de7b656dbeea16
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2021-06-19 11:46:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Task Tracker
+
+This is a simple task tracking app written in Angular 12.
+
+Example followed from https://github.com/bradtraversy/angular-crash-2021
+
+### Usage
+1. `npm install`
+2. `npm run server`
+3. `ng serve`
+
+![](screen-shot.png)
+
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable, ObservedValueOf} from 'rxjs';
+import { Task } from '../Task';
+
+const HTTP_OPTIONS = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  private apiUrl : string = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) { }
+
+  getTasks() : Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task) : Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, HTTP_OPTIONS);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, HTTP_OPTIONS);
+  }
+}
